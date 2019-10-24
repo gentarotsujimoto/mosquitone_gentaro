14431581f50cf6474679be68a90ab3f08a8ef4d2
gentarotsujimoto <gentaro.0827@gmail.com>
2019-10-25 01:03:44 +0900

    mypage_output変更

---
+++ clone.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="sheet.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="sheet">
+        <h1 class="text-left">Band Name: <?php print("".$_POST["BandName"]);?></h1>
+            <h2 class="text-right">Date:<?php print($_POST["datedata"]);?></h2>
+                <div class="row_1">
+                    <h3 class="text-center"><?php print($_POST["evntname"]);?></h3>
+                </div>
+    </section>
+</body>
+</html>

c31a0d8f9e24abaf3f5d7b8f7628f104c96eac20
gentarotsujimoto <gentaro.0827@gmail.com>
2019-10-26 14:06:56 +0900

    図形挿入用枠作成追加＋書式整理

--- clone.php
+++ clone.php
@@ -4,16 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="sheet.css">
+    <link rel="stylesheet" href="sheet1.css">
     <title>Document</title>
 </head>
 <body>
     <section class="sheet">
-        <h1 class="text-left">Band Name: <?php print("".$_POST["BandName"]);?></h1>
-            <h2 class="text-right">Date:<?php print($_POST["datedata"]);?></h2>
-                <div class="row_1">
-                    <h3 class="text-center"><?php print($_POST["evntname"]);?></h3>
-                </div>
+        <h1 class="text-left">Band Name:
+            <a1><?php print("".$_POST["BandName"]);?></a1>
+        </h1>
+        <h2 class="text-right">Date:
+            <?php print($_POST["datedata"]);?>
+        </h2>
+        <h3 class="text-right">
+                <?php print($_POST["place"]);?>
+        </h3>
+        <div class="row_1">
+            <h4 class="text-center">
+                <?php print($_POST["evntname"]);?>
+            </h4>
+        </div>
+        <br>
+        <div class="square"></div>
     </section>
+    
 </body>
 </html>

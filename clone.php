d349aa8fd6d7e5c535857eada0c21221f90d9359
gentarotsujimoto <gentaro.0827@gmail.com>
2019-10-27 21:50:37 +0900

    CSS完成+clone.php更新

--- clone.php
+++ clone.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="sheet1.css">
+    <link rel="stylesheet" href="sheet.css">
     <title>Document</title>
 </head>
 <body>
@@ -24,8 +24,74 @@
             </h4>
         </div>
         <br>
+        <h5>Set Set Illustration:</h5>
         <div class="square"></div>
+        
+    <br>
+        <h6>Member:</h6>
+    <div class="row_5">
+            <table class="detail">
+                <thead>
+                    <tr>
+                        <th class="item">Name</th>
+                        <th class="unit_price">Part</th>
+                        <th class="amount">Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="dataline">
+                        <td class="text-center"><?php print("".$_POST["membername[1]"]);?></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"><?php print("".$_POST["membername[2]"]);?></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    <tr class="dataline">
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                    </tr>
+                    </tbody>
+            </table>
+    </div>
     </section>
-    
 </body>
 </html>
